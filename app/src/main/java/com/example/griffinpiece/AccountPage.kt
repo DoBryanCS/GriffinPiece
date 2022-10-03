f0f8320961f1485b969deede53b1102a42b8bf5f
e1937223 <e1937223@cmaisonneuve.qc.ca>
2022-10-03 01:20:33 -0400

    AccountPage Start

---
+++ app/src/main/java/com/example/griffinpiece/AccountPage.kt
@@ -0,0 +1,11 @@
+package com.example.griffinpiece
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_page)
+    }
+}
