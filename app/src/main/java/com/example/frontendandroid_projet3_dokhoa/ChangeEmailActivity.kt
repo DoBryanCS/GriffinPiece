6222fb003cfbd434ab491225ef7b6f36b9879e56
DoBryanCS <Do.Bryan.CS@gmail.com>
2022-10-04 19:08:26 +0200

    Ajout des pages change email et change password

---
+++ app/src/main/java/com/example/frontendandroid_projet3_dokhoa/ChangeEmailActivity.kt
@@ -0,0 +1,33 @@
+package com.example.frontendandroid_projet3_dokhoa
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class ChangeEmailActivity : AppCompatActivity() {
+
+    private lateinit var actionBar: ActionBar
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_changeemail)
+
+        actionBar = supportActionBar!!
+        actionBar.title = "Changer l'email"
+
+        actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#040404")))
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+}
---
+++ app/src/main/java/com/example/frontendandroid_projet3_dokhoa/ChangePasswordActivity.kt
@@ -0,0 +1,33 @@
+package com.example.frontendandroid_projet3_dokhoa
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class ChangePasswordActivity : AppCompatActivity() {
+
+    private lateinit var actionBar: ActionBar
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_changepassword)
+
+        actionBar = supportActionBar!!
+        actionBar.title = "Changer le mot de passe"
+
+        actionBar.setBackgroundDrawable(ColorDrawable(Color.parseColor("#040404")))
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+}
