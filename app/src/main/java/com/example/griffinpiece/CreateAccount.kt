7a6143227c1bce20c5c8a8229304800f0c47bf36
e1937223 <e1937223@cmaisonneuve.qc.ca>
2022-10-05 17:40:32 -0400

    Modification Mathieu

--- app/src/main/java/com/example/griffinpiece/CreateAccount.kt
+++ app/src/main/java/com/example/griffinpiece/CreateAccount.kt
@@ -1,5 +1,6 @@
 package com.example.griffinpiece
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,11 +8,16 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class CreateAccount : AppCompatActivity() {
     private lateinit var actionBar: ActionBar
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,15 +27,40 @@ class CreateAccount : AppCompatActivity() {
 
         var formEmail: EditText = findViewById(R.id.loginEmail)
         var formUsername: EditText = findViewById(R.id.loginUsername)
-        var formPasswprd: EditText = findViewById(R.id.loginPassword)
+        var formPassword: EditText = findViewById(R.id.loginPassword)
         var clickHere: TextView = findViewById(R.id.clickHereSignUp)
 
-        var btnCreateAccount: Button = findViewById(R.id.btnLogin)
+
+        var btnCreateAccount: Button = findViewById(R.id.btnSignUp)
 
         var intentLoginAccount = Intent(this, LoginActivity::class.java)
         btnCreateAccount.setOnClickListener{
-            Toast.makeText(this,"Votre utilisateur a été crée! ${formEmail.getText()} ", Toast.LENGTH_SHORT).show()
+
+            val queue = Volley.newRequestQueue(this)
+
+            val url = "http://172.105.104.199:3000/auth/register"
+
+            val jsonobject = JSONObject()
+
+            jsonobject.put("email", formEmail.text)
+            jsonobject.put("password", formPassword.text)
+            jsonobject.put("username", formUsername.text)
+
+            val request = JsonObjectRequest(Request.Method.POST,url, jsonobject,
+                Response.Listener {
+                    Log.i("email",jsonobject.getString("email"))
+                    Log.i("username", jsonobject.getString("username"))
+                    intentLoginAccount.putExtra("email", jsonobject.getString("email"))
+                    intentLoginAccount.putExtra("username",jsonobject.getString("username"))
+                    this.startActivity(intentLoginAccount)
+                },
+
+                Response.ErrorListener {
+                    Log.i("ERROR", it.toString())
+                })
+            queue.add(request)
         }
+
 
         clickHere.setOnClickListener{
             this.startActivity(intentLoginAccount)
--- app/src/main/java/com/example/griffinpiece/LoginActivity.kt
+++ app/src/main/java/com/example/griffinpiece/LoginActivity.kt
@@ -3,11 +3,17 @@ package com.example.griffinpiece
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var actionBar: ActionBar
@@ -18,21 +24,49 @@ class LoginActivity : AppCompatActivity() {
         actionBar = supportActionBar!!
         actionBar.title = "Connecter-vous à votre compte"
 
+        var sentEmail = this.intent.getStringExtra("email")
+        var sentUsername = this.intent.getStringExtra("username")
+
         var loginEmail: EditText = findViewById(R.id.loginEmail)
         var loginUsername: EditText = findViewById(R.id.loginUsername)
         var loginPassword: EditText = findViewById(R.id.loginPassword)
         var clickHere: TextView = findViewById(R.id.clickHereSignUp)
 
-        var btnLogin: Button = findViewById(R.id.btnLogin)
+        var btnLogin: Button = findViewById(R.id.btnSignUp)
+        val intentUserLogin = Intent(this, AccountPage::class.java)
         var intentCreateAccount = Intent(this, CreateAccount::class.java)
 
+        loginEmail.setText(sentEmail)
+        loginUsername.setText(sentUsername)
+
         btnLogin.setOnClickListener{
-            Toast.makeText(this,"Votre utilisateur a été crée! ${loginEmail.getText()} ", Toast.LENGTH_SHORT).show()
-            val intentLogin = Intent(this ,AccountPage::class.java)
-            intentLogin.putExtra("Email",  loginEmail.getText())
-            intentLogin.putExtra("Username", loginUsername.getText())
-            this.startActivity(intentLogin)
+            Toast.makeText(this,"Votre utilisateur est connecté! ${loginEmail.getText()} ", Toast.LENGTH_SHORT).show()
+
+            val queue = Volley.newRequestQueue(this)
+
+            val url = "http://172.105.104.199:3000/auth/login"
+
+            val jsonobject = JSONObject()
+
+            jsonobject.put("email", loginEmail.text)
+            jsonobject.put("password", loginPassword.text)
+            jsonobject.put("username", loginUsername.text)
+
+            val request = JsonObjectRequest(
+                Request.Method.POST,url, jsonobject,
+                Response.Listener {
+                    Log.i("email",jsonobject.getString("email"))
+                    Log.i("username", jsonobject.getString("username"))
+                    intentUserLogin.putExtra("email", jsonobject.getString("email"))
+                    intentUserLogin.putExtra("username",jsonobject.getString("username"))
+                    this.startActivity(intentUserLogin)
+
+                }, Response.ErrorListener {
+                    Log.e("ERROR",it.toString())
+                })
+            queue.add(request)
         }
+
 
         clickHere.setOnClickListener{
             this.startActivity(intentCreateAccount)
