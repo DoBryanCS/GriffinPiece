111c42986b4a88a258e4789ef5de4f364497484c
ChadJad <e1929936@cmaisonneuve.qc.ca>
2022-11-09 13:53:07 -0800

    media player

--- app/src/main/java/com/example/griffinpiece/ui/episode/EpisodeFragment.kt
+++ app/src/main/java/com/example/griffinpiece/ui/episode/EpisodeFragment.kt
@@ -1,15 +1,19 @@
 package com.example.griffinpiece.ui.episode
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.griffinpiece.R
 
 class EpisodeFragment : Fragment() {
 
     private lateinit var viewModel: EpisodeViewModel
+    private lateinit var mediaPLayer: MediaPlayer
+    private lateinit var runnable: Runnable
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,4 +23,10 @@ class EpisodeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_episode_page, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstaceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val episodeViewModel = ViewModelProvider(this).get(EpisodeViewModel::class.java)
+
+
+    }
 }
