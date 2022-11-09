a461cfd97ba477494a1d03241c8a3d9d2cb9ffcb
ChadJad <e1929936@cmaisonneuve.qc.ca>
2022-11-09 13:39:15 -0800

    impostor

--- app/src/main/java/com/example/griffinpiece/ui/episode/EpisodeViewModel.kt
+++ app/src/main/java/com/example/griffinpiece/ui/episode/EpisodeViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.griffinpiece.ui.episode
 
-class EpisodeViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.griffinpiece.models.Episode
+import com.example.griffinpiece.repositories.EpisodeRepository
+import com.example.griffinpiece.repositories.ShowsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class EpisodeViewModel (private val app: Application, episodeId: Int) : AndroidViewModel(app) {
+    var episodeInfo = MutableLiveData<Episode>()
+
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            val episodeRepository = EpisodeRepository(getApplication())
+            episodeRepository.getEpisode(episodeId, episodeInfo)
+        }
+    }
+
+
+
 }
